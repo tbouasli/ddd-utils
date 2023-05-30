e0aec68752f30d9309e4f3706604c97542efb5c6
Thomas Bouasli <thomas.bouasli@gmail.com>
2023-05-30 17:32:10 -0300

    [feat] added validation

--- src/domain/Entity.ts
+++ src/domain/Entity.ts
@@ -1,4 +1,6 @@
+import { validate } from "class-validator";
 import { UniqueIdentifier } from "./UniqueIdentifier";
+import { DomainValidationError, Either, left, right } from "src/errors";
 
 const isEntity = (v: any): v is Entity<any> => {
   return v instanceof Entity;
@@ -12,8 +14,9 @@ const isEntity = (v: any): v is Entity<any> => {
 export abstract class Entity<T> {
   protected readonly _id: UniqueIdentifier;
 
-  constructor(id?: string) {
+  constructor(props: T, id?: string) {
     this._id = id ? new UniqueIdentifier(id) : new UniqueIdentifier();
+    Object.assign(this, props);
   }
 
   get id(): UniqueIdentifier {
@@ -34,5 +37,16 @@ export abstract class Entity<T> {
     }
 
     return false;
+  }
+
+  protected async validate(): Promise<Either<DomainValidationError, void>> {
+    const errors = await validate(this);
+
+    if (errors.length > 0) {
+      const firstConstraint = Object.values(errors[0].constraints)[0];
+      return left(new DomainValidationError(firstConstraint));
+    }
+
+    return right(null);
   }
 }
