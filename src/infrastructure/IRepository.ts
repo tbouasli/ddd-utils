8ed979b5211d8f585a48f61b7ad03395fc2b499d
tbouasli <tbouasli@lidercap.com.br>
2024-01-10 16:16:03 -0300

    added things =D

--- src/infrastructure/IRepository.ts
+++ src/infrastructure/IRepository.ts
@@ -1,4 +1,4 @@
 export interface iRepository<IAggregateRoot> {
     save(aggregateRoot: IAggregateRoot): Promise<void> | void;
-    getById(id: string): Promise<IAggregateRoot> | IAggregateRoot;
+    getById(id: string): Promise<IAggregateRoot | undefined> | IAggregateRoot | undefined;
 }
